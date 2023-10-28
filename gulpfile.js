1c9209108ed46b316e794d0adbb3722555d6c4bf
gafful07@gmail.com <gafful07@gmail.com>
2023-10-28 14:25:07 +0000

    every thing some infitiasie

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+function defaultTask(cb) {
+  console.log('Gulp is working!');
+  cb();
+}
+
+exports.default = defaultTask;
+
+const {src, dest, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function css() {
+  return src('./src/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./d/'));
+}
+
+exports.css = css;
+
+const pug = require('gulp-pug');
+
+function html() {
+  return src('./src/pug/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(dest('./d'));
+}
+
+exports.html = html;
+
+function watchFiles() {
+  watch('./src/scss/*.scss', css);
+  watch(['./src/pug/*.pug', './src/pug/*/*.pug'], html);
+}
+
+exports.watch = watchFiles;
