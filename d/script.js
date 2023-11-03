b278be4900f9478d7e97660641b788e8e82d1106
gafful07@gmail.com <gafful07@gmail.com>
2023-11-03 23:50:22 +0000

    plus ads juicy ads

--- d/script.js
+++ d/script.js
@@ -10,24 +10,28 @@ function estWstFunction(estWstParam){
 
     // if elements inside mcardImage does not have the index '0' and also children lenght === 0
     if(eachElement.previousElementSibling && estWstParam === 'previous' && eachElement.childElementCount !== 0){
-      
+
+      let withoutfirst = eachElement.splice
       //take the firstElementChild of those elements and put them into their previous parent Sibblings
-      eachElement.previousElementSibling.insertBefore(eachElement.firstElementChild, eachElement.previousElementSibling.firstChild);
-      console.log('true');
-    } else if (eachElement.nextElementSibling && estWstParam === 'next' && eachElement.childElementCount !== 0) {
-      console.log('true');
-      eachElement.insertBefore(eachElement.nextElementSibling.firstElementChild, eachElement.firstChild);
+      // eachElement.previousElementSibling.insertBefore(eachElement.firstElementChild, eachElement.previousElementSibling.firstChild);
+      // console.log('true');
+    } else if (estWstParam === 'next' && eachElement.childElementCount !== 0) {
+
+      
+      // console.log(mcardImages[0].previousElementSibling);
+      // eachElement.insertBefore(eachElement.previousElementSibling.firstElementChild, eachElement.firstChild);
     } /*else if (index === iLenght + 1){
       // eastWest.firstElementChild.style.visibility = 'hidden';
       // eastWest.lastElementChild.style.visibility = '';
     } else if (mcardImages[iLenght-2].children.length === 0) {
       eastWest.lastElementChild.style.visibility = 'hidden';
       eastWest.firstElementChild.style.visibility = '';
-    }
-    // console.log(iLenght);*/
+    }*/
   })
 }
 
+// let arr = [1, 2, 3, 4, 5, 6];
+// console.log(arr.shift());
 /*function estWstFunction() {
 
   for(let i = 0; i < mcardImages.length; i++) {
