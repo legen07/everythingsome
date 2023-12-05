b193402abab18b861a061dcf82f7497c0174533a
gafful07@gmail.com <gafful07@gmail.com>
2023-12-05 01:38:45 +0000

    Checking for site speed.

---
+++ d/module.js
@@ -0,0 +1,75 @@
+export let productList = {
+  products :
+  {
+    CameraBulb :{
+      proName : 'Camera Bulb',
+
+      proPrice : 340,
+
+      proBrief : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse accusantium debitis pariatur omnis vel ea tenetur perferendis et autem unde, quasi ducimus quibusdam veritatis,',
+      
+      proDescription : ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse accusantium debitis pariatur omnis vel ea tenetur perferendis et autem unde, quasi ducimus quibusdam veritatis, officia architecto quod necessitatibus enim optio repellendus eveniet officiis ab repellat. Cum qui omnis maxime eaque, et repudiandae aliquam nesciunt nam earum nemo rerum nostrum provident?',
+
+      images: ['car_pump_full_package.jpg', 'car_pump_graphical.jpg', 'car_pump_graphical1.jpg', 'car_pump.jpg']
+    },
+
+    /////////////////////////////////////////////////////////////////////
+
+    headPhone : {
+      proPrice : `Gh&#8373; 340`,
+      
+      proBrief : 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      
+      proName : 'Placed products',
+      
+      matNTags : '<b> good old a fucking bosting things </b>',
+      
+      proDescription : ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse accusantium debitis pariatur omnis vel ea tenetur perferendis et autem unde, quasi ducimus quibusdam veritatis, officia architecto quod necessitatibus enim optio repellendus eveniet officiis ab repellat. Cum qui omnis maxime eaque, et repudiandae aliquam nesciunt nam earum nemo rerum nostrum provident?',
+
+      images: ['place-one.png','place-two.png', 'place-three.png', 'place-four.png', 'place-five.png']
+    },
+
+    /////////////////////////////////////////////////////////////////////
+
+    'RubberSealant' : {
+      'proName' : 'Camera Bulb',
+
+      'proPrice' : 340,
+
+      'proBrief' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse accusantium debitis pariatur omnis vel ea tenetur perferendis et autem unde, quasi ducimus quibusdam veritatis,',
+      
+      'proDescription' : ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse accusantium debitis pariatur omnis vel ea tenetur perferendis et autem unde, quasi ducimus quibusdam veritatis, officia architecto quod necessitatibus enim optio repellendus eveniet officiis ab repellat. Cum qui omnis maxime eaque, et repudiandae aliquam nesciunt nam earum nemo rerum nostrum provident?',
+
+      'images': ['car_pump_full_package.jpg', 'car_pump_graphical.jpg', 'car_pump_graphical1.jpg', 'car_pump.jpg']
+    }, 
+
+    /////////////////////////////////////////////////////////////////////
+
+    'carGlass' :{
+      'proName' : 'Camera Bulb',
+
+      'proPrice' : 340,
+
+      'proBrief' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse accusantium debitis pariatur omnis vel ea tenetur perferendis et autem unde, quasi ducimus quibusdam veritatis,',
+      
+      'proDescription' : ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse accusantium debitis pariatur omnis vel ea tenetur perferendis et autem unde, quasi ducimus quibusdam veritatis, officia architecto quod necessitatibus enim optio repellendus eveniet officiis ab repellat. Cum qui omnis maxime eaque, et repudiandae aliquam nesciunt nam earum nemo rerum nostrum provident?',
+
+      'images': ['car_pump_full_package.jpg', 'car_pump_graphical.jpg, car_pump_graphical1.jpg', 'car_pump.jpg']
+    },
+
+    /////////////////////////////////////////////////////////////////////
+
+    'carPump' : {
+      'proName' : 'Camera Bulb',
+
+      'proPrice' : 340,
+
+      'proBrief' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse accusantium debitis pariatur omnis vel ea tenetur perferendis et autem unde, quasi ducimus quibusdam veritatis,',
+      
+      'proDescription' : ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse accusantium debitis pariatur omnis vel ea tenetur perferendis et autem unde, quasi ducimus quibusdam veritatis, officia architecto quod necessitatibus enim optio repellendus eveniet officiis ab repellat. Cum qui omnis maxime eaque, et repudiandae aliquam nesciunt nam earum nemo rerum nostrum provident?',
+
+      'images': ['car_pump_full_package.jpg', 'car_pump_graphical.jpg', 'car_pump_graphical1.jpg', 'car_pump.jpg']
+    }
+
+  }
+}
--- d/script.js
+++ d/script.js
@@ -1,6 +1,12 @@
-let mcardOverlay = document.querySelectorAll(".mcard-images-overlay > *");
-let mcardLength = mcardOverlay.length;
-let estWst = document.querySelectorAll('.m-east-west > *');
+function doc(param) {
+  return document.querySelector(param);
+}
+function docs(param) {
+  return document.querySelectorAll(param);
+}
+
+let mcardOverlay = docs('.mcard-images-overlay > *');
+let estWst = docs('.m-east-west > *');
 mcardOverlay[0].classList = 'index-js';
 
 function estWstFunction(estWstParam) {
@@ -43,12 +49,12 @@ estWst[1].classList.add('hidden-js');
 // navigation bottom animations //////////////////////////////////
 //////////////////////////////////////////////////////////////////
 
-let articleCard = document.querySelector('.article-card');
-let mainCard = document.querySelector('.main-card');
-let naviNav2 = document.querySelector('.navi-nav2');
-let naviNav1 = document.querySelector('.navi-nav1');
-let bottomLeft = document.querySelector(".navi-bottom .left"); 
-let bottomRight = document.querySelector(".navi-bottom .right"); 
+let articleCard = doc('.article-card');
+let mainCard = doc('.main-card');
+let naviNav2 = doc('.navi-nav2');
+let naviNav1 = doc('.navi-nav1');
+let bottomLeft = doc(".navi-bottom .left"); 
+let bottomRight = doc(".navi-bottom .right"); 
 
 function changePage(pageParam) {
   if(pageParam === "left" && naviNav1.firstElementChild.innerHTML === 'learn more'){
@@ -87,27 +93,103 @@ bottomLeft.classList.toggle('js');
 
 
 
-////////////////////////////////////////////////////////////////////////
-// Form manupulations///////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-let form = document.querySelector('.form-card');
-let divInp = document.querySelectorAll("div input");
+///////////////////////////////////////////////////////////////////////
+// Form manupulations//////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+
+let cartList = [];
+let form = doc('.form-card');
+let divInp = docsAll("div input");
 divInp = [...divInp];
 divInp.shift();
 
+function formOpener() {
+  form.classList.add('js');
 
-////////////////////////////////////////////////////////////////////////
-////// PLUS AND MINUS FUNCTION  ////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
+  console.log(cartList.length);
 
-let digit = document.querySelector('.brief-brief p');
-let units = document.querySelector('.brief-brief .price-maths div');
-let total = document.querySelector('.brief-total strong');
-let unitNum = 1;
-let bulkErrMsg =  document.querySelector('.bulk-err-msg');
-form.appendChild(bulkErrMsg);
+  if (cartList.length < 1) {
+  
+  let createDivBrief = document.createElement('div');
+  createDivBrief.classList.add('form-brief-content');
+  createDivBrief.innerHTML = `<div class="brief-brief"> ${doc('.mcard-images-overlay div').innerHTML} <div class="brief-side"><h3>${doc('.article-content .art-head h1').textContent}</h3><div><span>₵ </span><span class="digit"> </span></div><div class="price-maths"><button class="minus" style="color: rgb(170, 170, 170);">-</button><div>1</div><button class="plus">+</button></div></div></div><div class="brief-total"> <b>Total = ₵ &nbsp;</b><strong>${doc('.mcard-price strong').textContent}</strong></div>`;
 
-function plusAndMinus (pnmParam) {
+  formBriefContent.insertAdjacentElement('afterbegin', createDivBrief);
+  }
+}
+
+
+
+
+let dateInput = doc('.form-date input');
+let dateBtn = doc('a.fd');
+function dateFunction () {
+  let todayDate = Date();
+  let slicedDate = todayDate.slice(0, 11);
+  if(dateBtn.textContent === "today") {
+    dateInput.toggleAttribute("type");
+    dateInput.setAttribute('placeholder', slicedDate)
+    dateBtn.textContent = 'select date';
+    dateInput.setAttribute('disabled', 'true')
+  }
+  else if (dateBtn.textContent === "select date") {
+    dateInput.toggleAttribute("placeholder");
+    dateBtn.textContent = 'today';
+    dateInput.setAttribute('type', 'date')
+    dateInput.removeAttribute('disabled')
+  }
+}
+
+let requiredField = docsAll('input[required]')
+requiredField.forEach( each => {
+  let span = document.createElement('span');
+  span.className = "required-field";
+  each.parentElement.insertAdjacentElement('beforeend', span);
+  span.textContent = 'required';
+});
+
+//////////////////////////////////////////////////////////////
+///////// header expansion function //////////////////////////
+//////////////////////////////////////////////////////////////
+let navigation = doc('.navi-overlay');
+let header = doc('header');
+
+function headExpandFoo() {
+
+  // if (naviNav1.classList.contains('nl')){
+    header.classList.toggle('js');
+
+    window.scrollTo( {
+      top: -1000, 
+      behavior: 'instant'
+    })
+
+    setTimeout( function () {
+      naviNav2.parentElement.classList.toggle('scroll-js');
+    }, 200);
+  // }
+}
+
+
+//////////////////////////////////////////////////////////////
+////// PLUS AND MINUS FUNCTION  //////////////////////////////
+//////////////////////////////////////////////////////////////
+
+
+function plusAndMinus(pnmParam, d) {
+  
+  if (cartList.length > 0) {
+  
+    let digit = d.parentElement.parentElement.querySelector('.brief-side .digit');
+    let units = d.parentElement.parentElement.querySelector('.brief-side .price-maths div');
+    let total = d.parentElement.parentElement.parentElement.parentElement.querySelector('.brief-total strong');
+    let unitNum = 1;
+    let bulkErrMsg =  doc('.bulk-err-msg');
+    form.appendChild(bulkErrMsg);
+
+  console.log('plus and minus function');
+  
   let digitNum = Number(digit.textContent);
   unitNum = Number(units.textContent);
   let totalNum = Number(total.textContent);
@@ -131,51 +213,79 @@ function plusAndMinus (pnmParam) {
     total.textContent = totalNum - digitNum;
     units.textContent = unitNum - 1;
   }
+} else {
+  console.log('nothing nothing');
+}
 }
 
-if (unitNum < 2) {
-  console.log('color change should be triggeres');
-  units.previousElementSibling.style.color = '#aaa';
-} 
-
-
-
-
-// Clonging an first image in carousel then shortcuting it into the form brief 
-let formBrief = document.querySelector('.form-brief-content');
-let mImgCloned = mcardOverlay[0].firstElementChild.cloneNode(true);
-  mImgCloned.className = 'cloned-img';
-
-  formBrief.insertAdjacentElement('afterbegin', mImgCloned);
-
-let dateInput = document.querySelector('.form-date input');
-let dateBtn = document.querySelector('a.fd');
-function dateFunction () {
-  let todayDate = Date();
-  let slicedDate = todayDate.slice(0, 11);
-  if(dateBtn.textContent === "today") {
-    dateInput.toggleAttribute("type");
-    dateInput.setAttribute('placeholder', slicedDate)
-    dateBtn.textContent = 'select date';
-    dateInput.setAttribute('disabled', 'true')
+try{
+  if (unitNum < 2) {
+    units.previousElementSibling.style.color = '#aaa';
   }
-  else if (dateBtn.textContent === "select date") {
-    dateInput.toggleAttribute("placeholder");
-    dateBtn.textContent = 'today';
-    dateInput.setAttribute('type', 'date')
-    dateInput.removeAttribute('disabled')
-  }
-}
+} catch {}
 
-let requiredField = document.querySelectorAll('input[required]')
-requiredField.forEach( each => {
-  let span = document.createElement('span');
-  span.className = "required-field";
-  each.parentElement.insertAdjacentElement('beforeend', span);
-  span.textContent = 'required';
-})
 
+
+
+function copiedToCart(d) {
   
+  let createDivBrief = document.createElement('div');
+  createDivBrief.classList.add('form-brief-content');
+  createDivBrief.innerHTML ='<div class="brief-brief"><div class="brief-side"><h3></h3><div><span>₵ </span><span class="digit"> </span></div><div class="price-maths"><button class="minus" style="color: rgb(170, 170, 170);">-</button><div>1</div><button class="plus">+</button></div></div></div><div class="brief-total"> <b>Total = ₵ &nbsp;</b><strong></strong></div>';
+  
+  function dF(ele) {
+    return d.parentElement.parentElement.querySelector(ele);
+  }
+  function cF(ele) {
+    return createDivBrief.querySelector(ele);
+  }  
+
+
+  let proNameContent = dF('.pro-name h2').textContent;
+
+  let clonedProImg = dF('.pro-img img').cloneNode(true);
+  cF('.brief-brief').insertAdjacentElement('afterbegin', clonedProImg);
+  cF('.brief-brief h3').textContent = proNameContent;
+  let priceProduct = dF('.pro-price strong').textContent.slice(1, 6);
+  cF('.brief-brief span.digit').textContent = priceProduct;
+  cF('.brief-total strong').textContent = priceProduct;
+
+  if ( Number(doc('.brief-brief .digit')) > 3) {
+      bulkErrMsg.classList.add('js');
+      setTimeout( function() {
+        bulkErrMsg.classList.remove('js');
+      }, 9000);
+      return;
+  } else if (!cartList.includes(proNameContent)) {
+    doc('.form-brief').insertAdjacentElement("afterbegin", createDivBrief);
+  } else {
+    docsAll('.form-brief').forEach(each => {
+      if (each.querySelector('.brief-brief h3').textContent === proNameContent) {
+        plusAndMinus('plus', doc('.brief-brief .plus'));
+      }
+    })
+  }
+  cartList.unshift(proNameContent);
+
+
+  naviNav2.classList.add('cart'); 
+  naviNav2.lastElementChild.textContent =  cartList.length;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /////////////////////////////////////////////////
 ///// Click event Listener //////////////////////
 document.addEventListener( 'click', e =>{
@@ -190,16 +300,16 @@ document.addEventListener( 'click', e =>{
 
 
 // bottom navigation event and animation
-  if(d.classList.contains('navi-nav1') || d.classList.contains('nav-e'))
+  if(d.classList.contains('nl'))
     changePage("left");
 
-  else if(d.classList.contains('navi-nav2') || d.classList.contains('nav-e'))
-    changePage("right");
+  // else if(d.classList.contains('nr'))
+    // changePage("right");
 
 
 /// Opening forms 
   if (d.classList.contains('fo') || d.classList.contains('fo1'))
-    form.classList.add('js');
+    formOpener();
 
   else if (d.classList.contains("fc"))
     form.classList.remove('js');
@@ -208,10 +318,11 @@ document.addEventListener( 'click', e =>{
     dateFunction();
 
   /// Plus and minus inside the briefed form information. 
-  if (d.classList.contains('plus')) 
-    plusAndMinus('plus');
-  else if (d.classList.contains('minus'))
-    plusAndMinus('minus');
+  if (d.classList.contains('minus')) {
+    plusAndMinus('minus', d);
+  } else if (d.classList.contains('plus')) {
+    plusAndMinus('plus', d);
+  }
 
 
   /// input styles 
@@ -230,7 +341,21 @@ document.addEventListener( 'click', e =>{
     }
   })
 
+  ///////////////////////////////////////////////
+  //hahmburger move's event
+  if(d.classList.contains('ho'))
+    headExpandFoo();
+
+  if(d.classList.contains('pc')) 
+    copiedToCart(d);
+
+  if(d.classList.contains('po')) {
+    productOpener(d);
+  }
 });
+////// CLICK EVENTS END HERE 
+////////////////////////////////////////////////////////
+
 
 ////////////////////////////////////
 //Scroll event Listener ////////////
@@ -240,6 +365,50 @@ window.addEventListener('scroll', scrol => {
   if (fortyMedia.matches && window.scrollY > 100) {
     naviNav2.parentElement.classList.add('scroll-js');
   } else if (window.scrollY < 110) {
-    naviNav2.parentElement.classList.remove('scroll-js');
+    try{
+      naviNav2.parentElement.classList.remove('scroll-js');
+    }  catch{}
   }
 })
+
+
+import {productList} from './module.js';
+
+// console.log(productList.products);
+
+Object.entries(productList.products).map( ([keys, element]) => {
+  let name = element.proName;
+  let price = element.proPrice;
+  let brief = element.proBrief;
+  let images = element.images;
+
+  let div = document.createElement('div');
+  div.classList.add('product', 'po');
+  div.setAttribute('id', keys);
+  div.innerHTML = `<div class="product-back"> <div class="pro-img"> <img class="po" src="images/products/${images[0]}"><div class="pro-name"><h2>${name}</h2></div></div> <div class="pro-price"><strong>${price}</strong></div><div class="pro-cart pc"><div class="pc">+</div></div></div>`;
+
+  doc('.header-content').appendChild(div);
+});
+
+
+function productOpener(d) {
+  let clickedProduct = d.parentElement.parentElement.parentElement;
+  let clickedId = clickedProduct.getAttribute('id');
+
+  let _PPC = productList.products[clickedId];
+
+  let mCardElements = [...docsAll(' .art-head h1, .mcard-price strong, .mcard-description p, .article-content article p, .vit-material')];
+
+  for(let i = 0; i < _PPC.images.length; i++) {
+    let eachEle = document.createElement('img');
+    let eachPPC = _PPC.images[i];
+    eachEle.setAttribute('src', `./images/products/${eachPPC}`);
+    mcardOverlay[i].replaceChildren(eachEle);
+
+    mCardElements[i].innerHTML = Object.values(_PPC)[i];
+    console.log(Object.values(_PPC)[i]);
+    // console.log(mCardElements[i]);
+  }
+
+  headExpandFoo();
+}
